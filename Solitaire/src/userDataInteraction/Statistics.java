95b7e11cd49351779988c19650ef62bf3ae43847
Thatawesomeguy4 <derekteepee@gmail.com>
2018-11-18 13:02:45 -0500

    added stats object

---
+++ Solitaire/src/userDataInteraction/Statistics.java
@@ -0,0 +1,11 @@
+package userDataInteraction;
+
+public class Statistics 
+{
+	public static double timePlayed; //total time the user has spent playing the game
+	public static double fastestGame; //the least amount of time it took the user to win a game
+	public static int gamesPlayed; //number of new games started
+	public static int gamesWon; // number of games won
+	public static int gameScore; // users game score
+	public static int highScore; // records application highest score
+}

5da7794197bb49a4357e2f1409fc7a8a709a71ff
Thatawesomeguy4 <derekteepee@gmail.com>
2018-11-19 14:27:52 -0500

    added extra play button booleans

--- Solitaire/src/userDataInteraction/MenuCustomizationSettings.java
+++ Solitaire/src/userDataInteraction/MenuCustomizationSettings.java
@@ -14,4 +14,9 @@ public class MenuCustomizationSettings implements Serializable
 	public static boolean selectProfile; //bool to decide to/not to display select profile button.
 	public static boolean createProfile; //bool to decide to/not to display create profile button.
 	public static boolean stats; //bool to decide to/not to display stats button.
+	public static boolean playLBL;
+	public static boolean playCH;
+	public static boolean playSC;
+	public static boolean playSP;
+	public static boolean playPR;
 }
